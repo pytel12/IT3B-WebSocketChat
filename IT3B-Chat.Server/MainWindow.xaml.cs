da0d17a2f17072d1e5fa08920bad613bba65f721
Petr Valoušek <Valousek.Petr@stud.vassboskovice.cz>
2024-05-13 09:31:05 +0200

    čest tady je muj první pokus a tak no

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -8,17 +8,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
 
 namespace IT3B_Chat.Server
 {
- /// <summary>
- /// Interaction logic for MainWindow.xaml
- /// </summary>
- public partial class MainWindow : Window
- {
-  public MainWindow()
-  {
-   InitializeComponent();
-  }
- }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> messageLog;
+        private ObservableCollection<string> connectionLog;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            messageLog = new ObservableCollection<string>();
+            connectionLog = new ObservableCollection<string>();
+
+            // Simulace příchozích zpráv a událostí připojení/odpojení klientů
+            SimulateIncomingMessages();
+            SimulateConnectionEvents();
+        }
+
+        public ObservableCollection<string> MessageLog
+        {
+            get { return messageLog; }
+        }
+
+        public ObservableCollection<string> ConnectionLog
+        {
+            get { return connectionLog; }
+        }
+
+        private void SimulateIncomingMessages()
+        {
+            // Simulace příchozích zpráv
+            for (int i = 1; i <= 10; i++)
+            {
+                messageLog.Add($"Zpráva od klienta {i}: Obsah zprávy {i}.");
+            }
+        }
+
+        private void SimulateConnectionEvents()
+        {
+            // Simulace událostí připojení/odpojení klientů
+            for (int i = 1; i <= 5; i++)
+            {
+                connectionLog.Add($"Klient {i} se připojil.");
+            }
+            for (int i = 1; i <= 3; i++)
+            {
+                connectionLog.Add($"Klient {i} se odpojil.");
+            }
+        }
+    }
 }

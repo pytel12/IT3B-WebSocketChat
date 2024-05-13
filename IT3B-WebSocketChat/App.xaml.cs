14fbe89230168e11462494d238e06394531fd037
Jiří Hlaváček <jirka@jhlavacek.cz>
2024-05-13 08:29:43 +0200

    Přidejte soubory projektu.

---
+++ IT3B-WebSocketChat/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IT3B_WebSocketChat
+{
+ /// <summary>
+ /// Interaction logic for App.xaml
+ /// </summary>
+ public partial class App : Application
+ {
+ }
+
+}
